d1e5c4837b7235ab15bc62d45e2a15f8e922a4b2
mckinseyleckenby <leckenbym11@gmail.com>
2022-04-06 17:58:16 -0500

    commit tree 9d8b2fbe11b31eecc1f2f76aeff94b2526e0af26

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42
+    } else {
+        (blocks < 42)
+        return 42 - blocks
+    }
+}
+
+const distanceFromHqInFeet = function(blocks){
+    return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, end){
+    if(start > end){
+    return (start - end) * 264
+    }else{
+        return (end - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, end){
+    const distance = distanceTravelledInFeet (start, end)
+    if (distance < 400){
+        return 0
+    }else if(distance > 400 && distance < 2000){
+        return (distance - 400) * .02
+    }else if(distance > 2000 && distance < 2500 ){
+        return 25 
+        }else{
+        return 'cannot travel that far'
+    }
+}
+
