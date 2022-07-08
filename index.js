c124ccd5aa46f28461cdafbd65a8256777ba3cc3
ittehadzihan <zihan991@gmail.com>
2022-07-08 11:33:19 -0400

    DONE

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
-// Code your solution in this file!
+// Code your solution in this fil
 
-const startingRoad = 42;
-function distanceFromHqInBlocks(blocks){
- if (blocks >=42){
-   return (blocks - startingRoad)
- }   
-    else if (blocks <= 42){
-       return (startingRoad - blocks) 
-    }
- }
+function distanceFromHqInBlocks(D){
+   if(D >= 42){
+       return (D - 42)
+   }else if(D <= 42){
+       return (42 - D)
+   }
+}
 
- function distanceFromHqInFeet(blocks){
-     if (blocks >= 42){
-         return((blocks -startingRoad)* 264)
-     }
-     else if (blocks <= 42){
-         return((startingRoad -blocks)*264)
-     }
- }
+function distanceFromHqInFeet(destination){
+    let blocks = distanceFromHqInBlocks(destination)
+  return (blocks * 264)
 
- function distanceTravelledInFeet(start, destination){
-    if (destination > start){
-        return ((destination - start)*264) 
-    }
-     else if(start > destination){
-        return ((start - destination)*264)
-     }   
-  } 
- 
-  function calculatesFarePrice(start, destination){
-   const distance = distanceTravelledInFeet(start, destination)  
-    if (distance <= 400){
-        return 0
-    }
-    else if(distance > 400 && distance <= 2000){
-        return (.02 *(distance -400))
-    }
-    else if (distance > 2000 && distance < 2500){
-        return 25
-    }
-    else 
-        return"cannot travel that far"
+}
+
+function distanceTravelledInFeet(start, finish){
+   if (start >= finish){
+       let blocksTravelled = start - finish
+       return blocksTravelled *264
+   }else if(start <= finish){
+       let blocksTravelled = finish - start
+       return blocksTravelled * 264
+   }
+}
+
+function calculatesFarePrice(pickUpLocation, dropOfLocation){
+    let distanceInFeet = distanceTravelledInFeet(pickUpLocation,dropOfLocation)
+    let faresPrice = 0;
+    if (distanceInFeet < 400 ){
+        return faresPrice = 0;
+    } else if (distanceInFeet >= 400 && distanceInFeet < 2000){
+            return faresPrice = (distanceInFeet - 400) * 0.02
+    } else if (distanceInFeet >= 2000 && distanceInFeet <=2500){
+        return faresPrice = 25
+    } else {
+        return "cannot travel that far"
     }
 
+}
